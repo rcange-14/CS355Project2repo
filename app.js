bf86ef4c32dd5f2ee5b6887f36b8e9c629a784fb
rcange-14 <ralphcange2@gmail.com>
2026-06-23 22:10:52 -0400

    Clean up comments in app.js
    Removed commented sections for view engine setup and error handling.

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ var app = express();
   }
 })();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -45,14 +44,14 @@ app.use('/users', usersRouter);
 app.use('/tutor', tutorRouter);
 app.use('/profile', profileRouter);
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+ 
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
